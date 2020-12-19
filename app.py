deaa97045e20fcb07c24f8bdd81b024002f06791
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-12-19 15:05:39 +0100

    some improvments

--- app.py
+++ app.py
@@ -11,8 +11,15 @@ from shlex import quote
 import requests
 import threading
 
-class Restic():
+seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
 
+def convert_to_seconds(duration):
+    return int(duration[:-1]) * seconds_per_unit[duration[-1]]
+
+def convert_to_mseconds(duration):
+    return convert_to_seconds(duration) * 1000
+
+class Restic():
     def init(self):
         self.__run('init')
 
@@ -30,6 +37,7 @@ class Restic():
                 return False
             raise e
 
+    @retry(wait_exponential_multiplier=convert_to_mseconds('5s'), wait_exponential_max=convert_to_mseconds('1m'), stop_max_attempt_number=5)
     def __run(self, cmd, args = [], capture_output = False):
         print('RESTIC START', cmd, 'with args', args)
         try:
@@ -41,29 +49,29 @@ class Restic():
             print('RESTIC ERROR', e.returncode, e.stdout, e.stderr)
             raise e
 
+class Notifier():
+    def notify(self, event_name):
+        event = {
+            'ts': round(time.time()),
+            'event': event_name
+        }
+        threading.Thread(target=self.do_notify, args=(event,)).start()
+
+    @retry(wait_exponential_multiplier=convert_to_mseconds('1s'), wait_exponential_max=convert_to_mseconds('5m'), stop_max_attempt_number=30)
+    def do_notify(self, event):
+        print('Notifying', event)
+        requests.post(notify_url, json=event)
+
 restic = Restic()
+notifier = Notifier()
 scheduler = sched.scheduler()
 list_separator = ','
 backup_paths = os.environ['BACKUP_PATHS'].split(list_separator)
 backup_exclude = os.environ['BACKUP_EXCLUDE'].split(list_separator)
-backup_schedule = int(os.environ['BACKUP_SCHEDULE'])
+backup_schedule = convert_to_seconds(os.environ['BACKUP_SCHEDULE'])
 notify_url = os.environ['NOTIF_URL']
 
-# for the moment only success
-# notify async (idea)
-def anotify(event_name):
-    event = {
-        'ts': round(time.time()),
-        'event': event_name
-    }
-    threading.Thread(target=notif, args=(event,)).start()
-
-@retry(wait_exponential_multiplier=1000, wait_exponential_max=300000, stop_max_attempt_number=30)
-def notif(event):
-    print('Notifying', event)
-    requests.post(notify_url, data = event)
-
-@retry(wait_exponential_multiplier=5000, wait_exponential_max=3600000)
+@retry(wait_fixed=convert_to_mseconds('30m'))
 def init():
     try:
         print('Initializing')
@@ -71,23 +79,26 @@ def init():
             restic.init()
         print('Ready')
     except Exception as e:
-        print('Init error', e)
-        anotify('error')
+        print('Init error, will retry later', e)
+        notifier.notify('error')
         raise e
 
-@retry(wait_exponential_multiplier=5000, wait_exponential_max=3600000)
-def backup():
+def schedule_backups():
+    backup()
+    scheduler.run()
+
+@retry(wait_fixed=convert_to_mseconds('30m'))
+def _backup():
     try:
         print('Backuping')
         restic.backup(backup_paths, excludes=backup_exclude)
         print('Backup done !')
         scheduler.enter(backup_schedule, 1, backup)
-        anotify('backup-done')
+        notifier.notify('backup-done')
     except Exception as e:
-        print('Backup error', e)
-        anotify('error')
+        print('Backup error, will retry later', e)
+        notifier.notify('error')
         raise e
 
 init()
-backup()
-scheduler.run()
+schedule_backups()
