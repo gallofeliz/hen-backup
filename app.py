d599fb137b4db097a8546bdf1e6de601bc126ef8
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-12-19 20:56:03 +0100

    some changes !

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import os
 from shlex import quote
 import requests
 import threading
+import logging
 
 seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
 
@@ -27,78 +28,77 @@ class Restic():
         args = paths + map(lambda exclude : '--exclude=' + quote(exclude), excludes)
         self.__run('backup', args)
 
+    def check():
+        self.__run('check')
+
     def is_init(self):
         try:
             self.__run('check', [], True)
             return True
         except CalledProcessError as e:
-            print(e.stderr)
             if e.stderr.find('conn.Object: Object Not Found') != -1:
                 return False
             raise e
 
-    @retry(wait_exponential_multiplier=convert_to_mseconds('5s'), wait_exponential_max=convert_to_mseconds('1m'), stop_max_attempt_number=5)
+    # Need to retry everytime ? Only if a network problem, etc, but I can't identify it, so for now I comment retry
+    # @retry(wait_exponential_multiplier=convert_to_mseconds('5s'), wait_exponential_max=convert_to_mseconds('1m'), stop_max_attempt_number=5)
     def __run(self, cmd, args = [], capture_output = False):
-        print('RESTIC START', cmd, 'with args', args)
+        logging.debug('RESTIC START %s %s %s', cmd, 'with args', args)
         try:
             process_cmd_parts = ["restic"] + [cmd] + args
             process = subprocess.run(process_cmd_parts, capture_output=capture_output, check=True, text=True)
-            print('RESTIC SUCCESS', process.returncode, process.stdout, process.stderr)
+            logging.debug('RESTIC SUCCESS %s %s %s', process.returncode, process.stdout, process.stderr)
             return process
         except CalledProcessError as e:
-            print('RESTIC ERROR', e.returncode, e.stdout, e.stderr)
+            logging.debug('RESTIC ERROR %s %s %s', e.returncode, e.stdout, e.stderr)
             raise e
 
-class Notifier():
-    def notify(self, event_name):
-        event = {
-            'ts': round(time.time()),
-            'event': event_name
-        }
-        threading.Thread(target=self.do_notify, args=(event,)).start()
-
-    @retry(wait_exponential_multiplier=convert_to_mseconds('1s'), wait_exponential_max=convert_to_mseconds('5m'), stop_max_attempt_number=30)
-    def do_notify(self, event):
-        print('Notifying', event)
-        requests.post(notify_url, json=event)
-
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s')
 restic = Restic()
-notifier = Notifier()
 scheduler = sched.scheduler()
 list_separator = ','
 backup_paths = os.environ['BACKUP_PATHS'].split(list_separator)
 backup_exclude = os.environ['BACKUP_EXCLUDE'].split(list_separator)
 backup_schedule = convert_to_seconds(os.environ['BACKUP_SCHEDULE'])
-notify_url = os.environ['NOTIF_URL']
+check_schedule = convert_to_seconds(os.environ['CHECK_SCHEDULE'])
 
 @retry(wait_fixed=convert_to_mseconds('30m'))
 def init():
     try:
-        print('Initializing')
+        logging.info('Initializing...')
         if not restic.is_init():
             restic.init()
-        print('Ready')
+        logging.info('Initialized !')
     except Exception as e:
-        print('Init error, will retry later', e)
-        notifier.notify('error')
+        logging.exception('Init error, will retry later')
         raise e
 
-def schedule_backups():
+def schedule():
     backup()
+    scheduler.enter(check_schedule, 1, check)
     scheduler.run()
 
 @retry(wait_fixed=convert_to_mseconds('30m'))
-def _backup():
+def check():
     try:
-        print('Backuping')
-        restic.backup(backup_paths, excludes=backup_exclude)
-        print('Backup done !')
-        scheduler.enter(backup_schedule, 1, backup)
-        notifier.notify('backup-done')
+        logging.info('Checking...')
+        restic.check()
+        logging.info('Check done !')
+        scheduler.enter(check_schedule, 1, check)
     except Exception as e:
-        print('Backup error, will retry later', e)
-        notifier.notify('error')
+        logging.exception('Check error, will retry later')
+        raise e
+
+@retry(wait_fixed=convert_to_mseconds('30m'))
+def backup():
+    try:
+        logging.info('Backuping...')
+        restic.backup(backup_paths, excludes=backup_exclude)
+        logging.info('Backup done !')
+        scheduler.enter(backup_schedule, 1, backup)
+    except Exception as e:
+        logging.exception('Backup error, will retry later')
         raise e
 
 init()
-schedule_backups()
+schedule()
