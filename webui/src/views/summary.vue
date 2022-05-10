d56b60debc0687aa08794ef7c66def5c1a117172
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2022-05-10 13:13:40 +0200

    revamp summary page

--- webui/src/views/summary.vue
+++ webui/src/views/summary.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
+
+    Todo :
+       - Add warnings badges
+       - Line danger on links that have 1 or more failed (add "report" on failed errors ?)
+
     <div v-if="summary">
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm">
+
       <h1>Backups</h1>
-      <div v-for="(operations, backupName) in summary.backups" :key="backupName" class="status-element">
+      <div v-for="(operations, backupName) in summary.backups" :key="backupName" class="status-element" :class="{'border-danger': (operations.backup.lastEndedJob &&  operations.backup.lastEndedJob.state === 'failed') || (operations.prune.lastEndedJob && operations.prune.lastEndedJob.state === 'failed')}" :ref="'backup-' + backupName">
         <b-icon-building class="icon"></b-icon-building>
         {{backupName}}
         <p>
           Last backup :
           <span v-if="operations.backup.lastEndedJob">
             {{ operations.backup.lastEndedJob.endedAt | formatAgo }},
-            <span :class="{'badge badge-danger': operations.backup.lastEndedJob.state !== 'done' }" v-b-tooltip.hover :title="operations.backup.lastEndedJob.error">{{ operations.backup.lastEndedJob.state }}</span>
+            <span :class="{'badge badge-danger': operations.backup.lastEndedJob.state === 'failed' }" v-b-tooltip.hover :title="operations.backup.lastEndedJob.error">{{ operations.backup.lastEndedJob.state }}</span>
           </span>
           <span v-else>Unknown</span>
           <br />
@@ -37,7 +47,7 @@
           Last prune :
           <span v-if="operations.prune.lastEndedJob">
             {{ operations.prune.lastEndedJob.endedAt | formatAgo }},
-            <span :class="{'badge badge-danger': operations.prune.lastEndedJob.state !== 'done' }" v-b-tooltip.hover :title="operations.prune.lastEndedJob.error">{{ operations.prune.lastEndedJob.state }}</span>
+            <span :class="{'badge badge-danger': operations.prune.lastEndedJob.state === 'failed' }" v-b-tooltip.hover :title="operations.prune.lastEndedJob.error">{{ operations.prune.lastEndedJob.state }}</span>
           </span>
           <span v-else>Unknown</span>
           <br />
@@ -63,15 +73,21 @@
         </p>
       </div>
 
+    </div>
+
+    <div class="col-md-auto" style="min-width: 100px">
+    </div>
+
+    <div class="col-sm">
       <h1>Repositories</h1>
-      <div v-for="(operations, repositoryName) in summary.repositories" :key="repositoryName" class="status-element">
+      <div v-for="(operations, repositoryName) in summary.repositories" :key="repositoryName" class="status-element" :class="{'border-danger': (operations.checkRepository.lastEndedJob &&  operations.checkRepository.lastEndedJob.state === 'failed')}" :ref="'repository-' + repositoryName">
         <b-icon-server class="icon"></b-icon-server>
         {{repositoryName}} <span v-if="repositoriesStats[repositoryName] && Object.keys(repositoriesStats[repositoryName]).length > 0">(<span class="repostat" v-if="repositoriesStats[repositoryName].size">{{repositoriesStats[repositoryName].size | formatSize}}</span><span class="repostat" v-if="repositoriesStats[repositoryName].billing">{{repositoriesStats[repositoryName].billing | formatBilling}}</span>)</span>
         <p>
           Last check :
           <span v-if="operations.checkRepository.lastEndedJob">
             {{ operations.checkRepository.lastEndedJob.endedAt | formatAgo }},
-            <span :class="{'badge badge-danger': operations.checkRepository.lastEndedJob.state !== 'done' }" v-b-tooltip.hover :title="operations.checkRepository.lastEndedJob.error">{{ operations.checkRepository.lastEndedJob.state }}</span>
+            <span :class="{'badge badge-danger': operations.checkRepository.lastEndedJob.state === 'failed' }" v-b-tooltip.hover :title="operations.checkRepository.lastEndedJob.error">{{ operations.checkRepository.lastEndedJob.state }}</span>
           </span>
           <span v-else>Unknown</span>
           <br />
@@ -95,6 +111,9 @@
           <run-button text="Check" @click="runCheck(repositoryName, $event)" style="float: right"></run-button>
         </p>
       </div>
+    </div>
+  </div>
+</div>
 
     </div>
   </div>
@@ -141,8 +160,15 @@ export default {
       return 'in ' + moment(date).toNow(true)
     }
   },
-  created() {
+  async created() {
+    this.config = await this.foregroundClient.getConfig()
+
     this.retrieveSummary()
+
+    this.windowResizeHandler = () => this.redraw()
+
+    window.addEventListener('resize', this.windowResizeHandler)
+
     this.retrieveStats()
     // Use https://www.npmjs.com/package/express-ws to get realtime jobs changes ?
     this.timer = setInterval(() => {
@@ -150,8 +176,22 @@ export default {
     }, 5000)
   },
   methods: {
+    cleanDraw() {
+      document.querySelectorAll('.entities-lines').forEach(n => n.remove())
+    },
+    redraw() {
+      this.cleanDraw()
+
+      this.config.backups.forEach(backup => {
+        backup.repositories.forEach(repositoryName => {
+          this.drawLine(backup.name, repositoryName)
+        })
+      })
+    },
     async retrieveSummary() {
         this.summary = await this.backgroundClient.getSummary()
+
+        this.$nextTick(() => this.redraw())
     },
     async retrieveStats() {
         this.repositoriesStats = await this.backgroundClient.getRepositoriesStats()
@@ -173,26 +213,73 @@ export default {
       await this.foregroundClient.check(repository, priority)
 
       this.retrieveSummary()
+    },
+    drawLine(backup, repository) {
+      const getOffset = (el) => {
+        const rect = el.getBoundingClientRect();
+        return {
+          left: rect.left + window.pageXOffset,
+          top: rect.top + window.pageYOffset,
+          width: rect.width || el.offsetWidth,
+          height: rect.height || el.offsetHeight
+        };
+      }
+
+      const connect = (div1, div2, color, thickness) => {
+        const off1 = getOffset(div1);
+        const off2 = getOffset(div2);
+
+        const x1 = off1.left + off1.width;
+        const y1 = off1.top + off1.height / 2;
+
+        const x2 = off2.left;
+        const y2 = off2.top + off2.height / 2;
+
+        const length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+        const cx = ((x1 + x2) / 2) - (length / 2);
+        const cy = ((y1 + y2) / 2) - (thickness / 2);
+
+        const angle = Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI);
+
+        const htmlLine = "<div class='entities-lines' style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+
+        const p = document.createElement('div')
+        p.innerHTML = htmlLine
+
+        const line = p.querySelector('div')
+
+        document.body.append(line);
+      }
+
+      const d1 = this.$refs['backup-' + backup][0]
+      const d2 = this.$refs['repository-' + repository][0]
+      connect(d1, d2, 'grey', 2)
     }
   },
   data() {
     return {
         summary: null,
         timer: null,
+        windowResizeHandler: null,
+        config: null,
         repositoriesStats: {}
     }
   },
   beforeDestroy () {
     this.cancelAutoUpdate();
+    window.removeEventListener('resize', this.windowResizeHandler)
+    this.cleanDraw()
   }
 }
 </script>
 
 <style scoped>
+    .status-element.border-danger {
+      border-width: 3px;
+    }
     .status-element {
-        display: inline-block;
         border: 1px solid grey;
-        margin-right: 15px;
         margin-bottom: 15px;
         padding: 15px 10px 0 10px;
     }
